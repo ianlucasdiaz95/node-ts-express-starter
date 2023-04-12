983d705870f613c00d5f38fa1827ef88a3692236
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2023-04-12 00:06:17 -0300

    Added Swagger OpenAPI integration

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,23 +1,26 @@
 import express from 'express';
 import 'reflect-metadata'; // this shim is required
-import { createExpressServer, useContainer } from 'routing-controllers';
+import { RoutingControllersOptions, createExpressServer, getMetadataArgsStorage, useContainer } from 'routing-controllers';
 import { MorganMiddleware, ErrorMiddleware, RateMiddleware } from '@/middlewares';
 import { PORT } from '@/config'
 import { UserController, RoleController } from '@/controllers';
 import { Container } from 'typedi';
 import { dataSource } from '@/db/connection';
+import { swaggerSpec } from '@/swagger';
+
 // required by routing-controllers
 useContainer(Container);
 
 export class Server {
     app: express.Express
     port: string
+    routingControllersOptions: any
 
     constructor(){
         this.port = PORT
-        this.app = createExpressServer({
+        this.routingControllersOptions = {
             controllers: [
-                UserController, 
+                UserController,
                 RoleController
             ],
             routePrefix: '/api',
@@ -30,17 +33,22 @@ export class Server {
                 RateMiddleware
             ],
             defaultErrorHandler: false,
-        })
+        }
 
     }
 
     async init(){
+
+        this.app = createExpressServer(this.routingControllersOptions)
 
         //Database connection
         await this.dbConnection()
 
         //Mount app
         await this.listen();
+
+        //Init docs
+        this.initDocs();
     }
 
     async dbConnection(){
@@ -50,6 +58,17 @@ export class Server {
             console.log('Database is up');
         } catch (error) {
             console.log('Database error: ', error);
+        }
+        
+    }
+
+    initDocs(){
+        try {
+            swaggerSpec(getMetadataArgsStorage, this.routingControllersOptions, this.app);
+
+            console.log('OpenAPI docs running on /docs');
+        } catch (error) {
+            console.log(error);
         }
         
     }
