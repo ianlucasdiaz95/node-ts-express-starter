cc780a90b391f8daaf16cecfa8b9c0a440175d6f
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2023-04-11 21:38:15 -0300

    updated DataSource declarations

--- db/connection.ts
+++ db/connection.ts
@@ -1,9 +1,10 @@
 import { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } from '../config'
 import { Log, Role, User } from '../entities';
-import { DataSource } from "typeorm"
+import { DataSource, DataSourceOptions } from "typeorm"
+import { SeederOptions } from 'typeorm-extension';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
-export const dataSource = new DataSource({
+const options: DataSourceOptions & SeederOptions = {
     type: "mysql",
     host: DB_HOST,
     port: parseInt(DB_PORT),
@@ -18,4 +19,8 @@ export const dataSource = new DataSource({
     logging: false,
     synchronize: true,
     namingStrategy: new SnakeNamingStrategy(),
-})
+    seeds: ['db/seeds/**/*{.ts,.js}'],
+    factories: ['db/factories/**/*{.ts,.js}']
+}
+
+export const dataSource = new DataSource(options)
